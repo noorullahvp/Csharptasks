0ec7b9ecc648a93217504ac9506cff985e80c169
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:58:08 +0530

    ty

---
+++ passbyvalue&reference/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program
+{
+
+    static void addanumber( int n)
+    {
+        n = 100;
+    }
+    static void Main()
+    {
+        int a = 10;
+        addanumber( a);
+        Console.WriteLine(a);
+        int s = 20;
+        addNumbers2(ref s);
+        Console.WriteLine(s);
+    }
+
+    static void addNumbers2(ref int s)
+    {
+        s = 500;
+    }
+
+   
+
+}
