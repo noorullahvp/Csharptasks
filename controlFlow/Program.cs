bbd8100cf842c8656680b0a35113085035f5e47a
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:54:47 +0530

    ipdate

---
+++ controlFlow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class  Program
+{
+    static void Main()
+    {
+        int number = -1;
+        if (number >0)
+        {
+            Console.WriteLine("number is positive");
+        }
+        if (number <0)
+        {
+            Console.WriteLine("number is negative");
+        } else
+        {
+            Console.WriteLine("number is positive");
+        } 
+
+    }
+}
