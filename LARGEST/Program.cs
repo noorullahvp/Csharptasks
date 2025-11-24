ddd25e67418769bb8f44fae41e371ce3123e0003
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:56:12 +0530

    yy

---
+++ LARGEST/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>() { 1, 2, 3, 5, 6, 7, 8 };
+        var orde = numbers.OrderByDescending(n => n)
+                          .Skip(1).First();
+                          
+        {
+            Console.WriteLine(orde);
+        }
+
+    }
+}
