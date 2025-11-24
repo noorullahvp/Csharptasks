bd0b5fa8bc898162ac5932fce1dcf82a27433dd4
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:53:40 +0530

    update

---
+++ abstraction/Program.cs
@@ -0,0 +1,39 @@
+﻿using abstraction;
+using System;
+
+
+
+ abstract class vehicle
+{
+      public abstract void Start();
+      public string name;
+    
+}
+
+class engine:Interface1
+{
+    public void stop()
+    {
+        Console.WriteLine("engine stoped");
+    }
+}
+
+class car : vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine($"{name}car started");
+    }
+}
+ class Program
+{
+        public static void Main()
+    {
+        car myCar = new car();
+        myCar.name = "swift";
+        myCar.Start();
+        engine myEngine = new engine();
+        myEngine.stop();
+    }
+
+}
