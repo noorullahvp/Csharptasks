adcfd978ef27e479e98055361dfb8d19a56b5611
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:55:33 +0530

    tyty

---
+++ inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class  Animal
+{
+    public string name;
+
+    public void bite()
+    {
+        Console.WriteLine($"{name} is bitting");
+    }
+}
+
+class  Dog:Animal
+{
+    public void bark()
+    {
+        Console.WriteLine($"{name} is barking");
+    }
+
+    static void Main()
+    {
+        Dog MyDog = new Dog();
+        MyDog.name = "tommy";
+        MyDog.bark();
+        MyDog.bite();
+    }
+}
