81563a98143a7b07168f49bc128a2cddd3b30b4b
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:57:30 +0530

    tyyt

---
+++ LINQ3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 5, 10, 2, 4, 1, 8, 6 };
+
+        var OrderOn = numbers.OrderByDescending(s => s);
+
+       foreach(var ord in OrderOn)
+        {
+            Console.WriteLine(ord);
+        }
+        var ordeIt = numbers.OrderBy(n => n);
+
+        foreach(var number in ordeIt)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
