454bbe71369e4cd02929c5e0308a4bf55c8e5e78
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:57:41 +0530

    ty

---
+++ LINQ5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class   Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 4, 6, 7, 8, 8, 9, 10 };
+
+        var df = numbers.First(n => n > 5);
+        
+       
+        {
+            Console.WriteLine(df);
+        }
+    }
+}
