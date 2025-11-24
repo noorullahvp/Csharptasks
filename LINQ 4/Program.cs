bf7521a971a7b7859e36665d9dd3ece98bd44ed7
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:56:53 +0530

    ty

---
+++ LINQ 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class student
+{
+    public string name { get; set; }
+    public int Mark { get; set; }
+
+}
+
+class program
+{
+    static void Main()
+    {
+        var students = new List<student>()
+        {
+            new student { name ="noor", Mark= 60},
+            new student{name ="ameen",Mark=60 },
+            new student{name="safa",Mark=90 },
+            new student{name="basith",Mark=90 }
+        };
+
+        var orderd = students.GroupBy(o => o.Mark);
+                             
+
+       foreach(var st in orderd)
+        {
+            Console.WriteLine($"order name is {st.name} order mark is {st.Mark}");
+        }
+
+    }
+}
