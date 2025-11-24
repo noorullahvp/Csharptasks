8501179f78df81d39baabbdc29b4b547e031a1ee
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:55:04 +0530

    upfate

---
+++ datatypes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+
+class Program
+{
+    static void Main()
+    {
+        short num = 232;
+        int num2 = 344;
+        long num3 = 4343;
+        BigInteger phone = 3239884434443434;
+        char letter = 'A';
+
+    }
+}
