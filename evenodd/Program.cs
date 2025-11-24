313df719bbbe447938534b9b3972e1e2226991e7
muhammed noorullah <muhammednoorullahvp@gmail.com>
2025-11-24 20:55:19 +0530

    iii

---
+++ evenodd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        var even = numbers.Where(n => n % 2 == 0);
+
+        foreach(var n in even)
+        {
+            Console.WriteLine(n);
+        }
+
+    }
+
+}
